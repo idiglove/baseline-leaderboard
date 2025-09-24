e974f7d12e95482f4b1642051fe1865bff089225
Faith Morante <faith.morante@gmail.com>
2025-09-24 15:19:15 -0700

    with resolve node

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
@@ -9,7 +10,7 @@ export default defineConfig({
     sourcemap: true
   },
   rollupOptions: {
-    input: './index.html'
+    input: resolve(__dirname, 'index.html')
   },
   server: {
     port: 3000,
