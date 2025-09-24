a9e46e8022e43f6e2ccbef51e376b205a34775f9
Faith Morante <faith.morante@gmail.com>
2025-09-24 15:21:29 -0700

    keep vite config short

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,4 @@ import { resolve } from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  },
-  rollupOptions: {
-    input: resolve(__dirname, 'index.html')
-  },
-  server: {
-    port: 3000,
-    host: true
-  }
 })
